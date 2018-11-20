e05e8d6eba620ef75c038635b39b71b7a41f2dbe
Aaron Graves <agraves1@ucvts.tec.nj.us>
2018-11-20 14:48:00 -0500

    first commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,13 +1,120 @@
-/**
- * Just like last time, the BankAccount class is primarily responsible
- * for depositing and withdrawing money. In the enhanced version, there
- * will be the added requirement of transfering funds between accounts.
- * 
- * Most of the functionality for this class should have already been
- * implemented last time. You can always reference my Github repository
- * for inspiration (https://github.com/rwilson-ucvts/java-sample-atm).
- */
-
 public class BankAccount {
 	
+	private static long generatedAccountNumber = 100000001L;
+	
+	private long accountNumber;
+	private double balance;
+	private User accountHolder;
+	
+	/**
+	 * Constructor for BankAccount class.
+	 * 
+	 * @param balance
+	 * @param user
+	 */
+	
+	public BankAccount(double balance, User accountHolder) {
+		this.accountNumber = BankAccount.generatedAccountNumber++;
+		this.balance = balance;
+		this.accountHolder = accountHolder;
+	}
+	
+	/////////////////////////////////// GETTERS AND SETTERS ///////////////////////////////////
+	
+	/**
+	 * Retrieves the account number.
+	 * 
+	 * @return accountNumber
+	 */
+	
+	public long getAccountNumber() {
+		return accountNumber;
+	}
+	
+	/**
+	 * Retrieves the balance.
+	 * 
+	 * @return balance
+	 */
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	/**
+	 * Retrieves the user.
+	 * 
+	 * @return user
+	 */
+	
+	public User getAccountHolder() {
+		return accountHolder;
+	}
+	
+	/**
+	 * Sets the value of accountNumber.
+	 * 
+	 * @param accountNumber the new account number
+	 */
+	
+	public void setAccountNumber(long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	
+	/**
+	 * Sets the value of balance.
+	 * 
+	 * @param balance the new balance
+	 */
+	
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	/**
+	 * Sets the value of user.
+	 * 
+	 * @param user the new user
+	 */
+	
+	public void setaccountHolder(User accountHolder) {
+		this.accountHolder = accountHolder;
+	}
+	
+	/////////////////////////////////// INSTANCE METHODS ///////////////////////////////////
+	
+	/**
+	 * Deposits money into this account.
+	 * 
+	 * @param amount the money to deposit
+	 * @return a status code (0: invalid amount, 1: success)
+	 */
+	
+	public int deposit(double amount) {
+		if (amount <= 0) {
+			return 0;
+		} else {
+			balance = balance + amount;
+			
+			return 1;
+		}
+	}
+	
+	/**
+	 * Withdraws money from this account.
+	 * 
+	 * @param amount the money to withdraw
+	 * @return a status code (0: insufficient funds, 1: invalid amount, 2: success)
+	 */
+	
+	public int withdraw(double amount) {
+		if (amount > balance) {
+			return 0;
+		} else if (amount <= 0) {
+			return 1;
+		} else {
+			balance = balance - amount;
+			return 2;
+		}
+	}
 }
--- src/User.java
+++ src/User.java
@@ -11,4 +11,156 @@
 
 public class User {
 	
+	private int pin;
+	private String name;
+	private String dob;
+	private String address;
+	private String city;
+	private String phone;
+	private String state;
+	private String postal;
+	
+	/**
+	 * Constructor for User class.
+	 * 
+	 * @param name
+	 * @param dob
+	 * @param address
+	 */
+	
+	public User(int pin, String name, String dob, String address, String city, String phone, String state, String postal) {
+		this.pin = pin;
+		this.name = name;
+		this.dob = dob;
+		this.address = address;
+		this.city = city;
+		this.phone = phone;
+		this.state = state;
+		this.postal = postal;
+	}
+	
+	/////////////////////////////////// GETTERS AND SETTERS ///////////////////////////////////
+	
+	/**
+	 * Retrieves the user's PIN.
+	 * 
+	 * @return pin
+	 */
+	
+	public int getPIN() {
+		return pin;
+	}
+	
+	/**
+	 * Retrieves the user's name.
+	 * 
+	 * @return name
+	 */
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getState() {
+		return state;
+	}
+
+	public String getPostal() {
+		return postal;
+	}
+	
+	/**
+	 * Retrieves the user's phone
+	 * 
+	 * @return name
+	 */
+	
+	public String getPhone() {
+		return phone;
+	}
+	/**
+	 * Retrieves the user's date of birth.
+	 * 
+	 * @return dob
+	 */
+	
+	public String getDOB() {
+		return dob;
+	}
+	
+	/**
+	 * Retrieves the user's address.
+	 * 
+	 * @return address
+	 */
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	/**
+	 * Sets the value of the user's PIN.
+	 * 
+	 * @param pin the new PIN
+	 */
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public void setPIN(int pin) {
+		this.pin = pin;
+	}
+	
+	/**
+	 * Sets the value of the user's name.
+	 * 
+	 * @param name the new name
+	 */
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Sets the value of the user's phone.
+	 * 
+	 * @param name the new name
+	 */
+	
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	/**
+	 * Sets the value of the user's date of birth.
+	 * 
+	 * @param dob the new date of birth
+	 */
+	
+	public void setDOB(String dob) {
+		this.dob = dob;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setPostal(String postal) {
+		this.postal = postal;
+	}
+	/**
+	 * Sets the value of the user's address.
+	 * 
+	 * @param address the new address
+	 */
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
 }
