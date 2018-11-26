15b46136b6d9e30cdc1ae841ed417c0d6ca0ee44
Aaron Graves <agraves1@ucvts.tec.nj.us>
2018-11-26 14:36:37 -0500

    updoot

--- src/Database.java
+++ src/Database.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * This class will serve as the intermediary between our ATM program and
  * the database of BankAccounts. It'll be responsible for fetching accounts
@@ -7,4 +12,17 @@
 
 public class Database {
 	
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+	
+		try(BufferedReader br = new BufferedReader(new FileReader("accounts-db.txt"))) {
+		 	String line;
+		 while ((line = br.readLine()) !=null) {
+		 }
 }
+		//try {
+		//    Database.write(Paths.get("myfile.txt"), "the text".getBytes(), StandardOpenOption.APPEND);
+	//	}catch (IOException e) {
+		//    //exception handling left as an exercise for the reader
+		}
+}
+//}
